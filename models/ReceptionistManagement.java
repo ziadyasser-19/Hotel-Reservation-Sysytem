aa7a0c24bc7eaddac9a8d405b001be162f2785b3
Farah Hossam <farah.alden2004@gmail.com>
2024-01-27 21:46:32 +0200

    receptionist management

--- models/ReceptionistManagement.java
+++ models/ReceptionistManagement.java
@@ -6,19 +6,55 @@ public class ReceptionistManagement {
     private static ArrayList<Receptionist> receptionistArray= new ArrayList<>();
 
     // Methods
-    public void addEmployee(String name, int pass){
+    public static int addEmployee(String name, int pass){
+        for(int i = 0; i<receptionistArray.size(); i++){
+            if(receptionistArray.get(i).getPass()==pass){
+                return -1; // password used already
+            }
+        }
+        receptionistArray.add(new Receptionist(name, pass));
+        return 1; // New Receptionist added successfully
+    }
+
+
+    public static int updateEmployeePass(int id, int pass){
+        for(int i = 0; i<receptionistArray.size(); i++){
+            if(receptionistArray.get(i).getPass()==pass){
+                return -2; // password already used
+            }
+        }
+        for(int i = 0; i<receptionistArray.size(); i++){
+            if(receptionistArray.get(i).getID()==id ){
+                receptionistArray.get(i).setPass(pass);
+                return 1; // Receptionist's Pass updated successfully
+            }
+        }
+        return -1; // Receptionist not found
+    }
+
+
+    public static int deleteEmployee(int id){
+        for(int i = 0; i<receptionistArray.size(); i++){
+            if(receptionistArray.get(i).getID() == id){
+                receptionistArray.remove(i);
+                return 1; // Receptionist removed successfully
+            }
+        }
+        return -1; // Receptionist not found
+    }
+
+
+    public static Receptionist search(int id){
+        for(int i =0; i<receptionistArray.size(); i++){
+            if(receptionistArray.get(i).getID()==id){
+                return receptionistArray.get(i);
+            }
+        }
+        return null; // Receptionist not found
+    }
+
     
-    }
-
-    public void updateEmployee(int id){
-
-    }
-
-    public void deleteEmployee(int id){
-
-    }
-
-    public void search(int id){
-
+    public static ArrayList<Receptionist> getAllReceptionists(){
+        return receptionistArray;
     }
 }
