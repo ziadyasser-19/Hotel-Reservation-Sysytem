2a3a811d7143f96124e3d2f3ccc3db710063badd
ziadyasser-19 <ziadyasser1564@gmail.com>
2024-02-02 18:02:22 +0200

    added last

---
+++ helpers/Pathes.java
@@ -0,0 +1,13 @@
+package helpers;
+
+public class Pathes {
+    
+    static private final String base = "Files";  // if there is an error delete src/
+    public static String adminspathes = base + "/admins.txt";
+    public static String RoomPath = base + "/Rooms.txt";
+    public static String ReceptionistPath = base + "/Receptionists.txt";
+    public static String deletedIDpath = base + "/DeletedId.txt";
+    public static String  GuestsPath= base+ "/guests.txt";
+    public static String  ServicesPath= base+ "/Services.txt";
+    
+}
